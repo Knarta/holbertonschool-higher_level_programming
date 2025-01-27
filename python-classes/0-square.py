a6ad7ccd92200dd3b77b1a3dee4b690ff017c909
root <root@Chacha.>
2025-01-27 17:09:15 +0100

    empty class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a class Square"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self):
+        """Initializes the Square instance"""
+        self
