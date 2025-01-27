13f0dd7c3055091e5b346106ab95d98de1daa7d7
root <root@Chacha.>
2025-01-27 19:34:15 +0100

    Modified code test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -44,12 +44,13 @@ class Square:
         """ Method that prints a square """
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for j in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+
+        for i in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
