e1f8d582a747b5502c1158d581b5a953c991db51
root <root@Chacha.>
2025-01-27 19:40:39 +0100

    Modified the code for print line in square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,8 +46,8 @@ class Square:
             print()
             return
 
-        for i in range(self.__position[1]):
+        for _ in range(self.__position[1]):
             print()
 
-        for i in range(self.__size):
+        for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
