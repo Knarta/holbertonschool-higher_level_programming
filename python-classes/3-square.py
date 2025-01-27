5cd9e6426e8022dd89d1a661c23f9a25a01608ca
root <root@Chacha.>
2025-01-27 18:39:10 +0100

    Add square that define area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that defines a class Square """
+
+
+class Square:
+    """ Class that defines a square """
+    __size = None
+
+    def area(self):
+        """ Method that returns the area of the square """
+        return self.__size * self.__size
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
