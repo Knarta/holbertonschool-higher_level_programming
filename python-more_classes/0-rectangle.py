beeaf613e00dc3074ea3a8b040d0cdbcb1ee0e18
root <root@Chacha.>
2025-01-28 15:11:44 +0100

    Class rectangle tha defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module that defines a class Rectangle """
+
+
+class Rectangle:
+    """ Class that defines a rectangle """
+    def __init__(self):
+        """ Initializes the Rectangle instance """
+        self
