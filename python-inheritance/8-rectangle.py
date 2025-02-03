a0c50688210cb0390f8bcbde00d92eb0c2ddaa73
root <root@Chacha.>
2025-02-03 23:55:48 +0100

    function class Rectangle that inherits from BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
         """Initialization of the rectangle class"""
-        
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
