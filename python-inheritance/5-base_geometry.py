038d9e5c8af84f5fdf2adbba2522ee3d45145c29
root <root@Chacha.>
2025-02-04 00:49:31 +0100

    Modified the code because I had a task confusion

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -3,7 +3,5 @@
 
 
 class BaseGeometry:
-    """A class with a method area() that is not implemented"""
-    def area(self):
-        """Method that is not implemented"""
-        raise Exception("area() is not implemented")
+    """Empty class BaseGeometry"""
+    pass
