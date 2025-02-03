59789c6824ebec90a34246ac6fa421b967299852
root <root@Chacha.>
2025-02-03 23:18:18 +0100

    Function BaseGeometry improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class."""
+    def area(self):
+        """Method that raises an Exception with the message
+        'area() is not implemented'."""
+        raise Exception("area() is not implemented")
