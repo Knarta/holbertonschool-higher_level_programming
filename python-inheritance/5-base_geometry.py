77a2654c3ba70ffbec6c31f628e843e3e45c0ba5
root <root@Chacha.>
2025-02-03 23:03:31 +0100

    Function write an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """A class with a method area() that is not implemented"""
+    def area(self):
+        """Method that is not implemented"""
+        raise Exception("area() is not implemented")
