37aee3584dda36d34819e87187e61d5682939b62
root <root@Chacha.>
2025-02-03 22:40:27 +0100

    Modified the code because pycodestyle

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module to check if object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if object is an instance of a class"""
+    return isinstance(obj, a_class)
