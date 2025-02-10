a59b02d8828b572ab6c8396be9ec84e05f315596
root <root@Chacha.>
2025-02-10 11:00:14 +0100

    Modification because the output is fail

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-Read a text file (UTF8) and print it to stdout
+Module that reads a text file (UTF8) and prints it to stdout
 """
 
 
 def read_file(filename="my_file_0.txt"):
-    with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+    Read a text file (UTF8) and print it to stdout
+    """
+    with open(filename, 'r', encoding='utf-8') as my_file_0:
+        print(my_file_0.read(), end="")
