5e6da745fa87d16dafeb2759715a95706209bff3
root <root@Chacha.>
2025-02-10 19:31:11 +0100

    Create the class Student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""
+Module that defines a Student class
+"""
+
+
+class Student:
+    """Defines a Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns the dictionary description with simple data structure"""
+        return self.__dict__
