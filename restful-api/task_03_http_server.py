b3d379f4245e2802298c13613b837d1ac19fe8a4
knarta <charquen10@gmail.com>
2025-02-18 21:32:11 +0100

    Refactor request handling in HTTP server to improve response structure and ensure proper JSON formatting

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,36 +12,36 @@ class RequestHandler(BaseHTTPRequestHandler):
         if self.path == '/data':
             data = requests.get('https://jsonplaceholder.typicode.com/posts')
             statuscode = data.status_code
-            json_data = json.dumps(data)
+            json_data = json.dumps(data.json())
             if statuscode == 200:
                 self.send_response(200)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(json_data.encode("utf-8"))
-            elif self.path == '/':
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b"Hello, a simple API")
-            elif self.path == '/status':
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b"OK")
-            elif self.path == '/info':
-                json_info = json.dumps({
-                    "status": statuscode,
-                    "message": "This is a simple API"
-                })
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(json_info.encode("utf-8"))
-            else:
-                self.send_response(404)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(b"404 Not Found")
+        elif self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"Hello, a simple API")
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == '/info':
+            json_info = json.dumps({
+                "status": 200,
+                "message": "This is a simple API"
+            })
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json_info.encode("utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
 
 
 host = 'localhost'
