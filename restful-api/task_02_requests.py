3d43d78fa82c9d1602d54dbe3b18eacefbe39171
knarta <charquen10@gmail.com>
2025-02-18 20:59:31 +0100

    Fix variable name for status code in task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,14 +8,14 @@ import csv
 """Fetch posts from the API"""
 
 response = requests.get('https://jsonplaceholder.typicode.com/posts')
-status_code = response.status_code
+statuscode = response.status_code
 
 """Function to fetch and print posts"""
 
 
 def fetch_and_print_posts():
-    print(f"Status code: {status_code}")
-    if status_code == 200:
+    print(f"Status code: {statuscode}")
+    if statuscode == 200:
         for post in response.json():
             print(post["title"])
 
@@ -24,7 +24,7 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
-    if status_code == 200:
+    if statuscode == 200:
         posts = response.json()
         with open("posts.csv", mode="w", newline='') as file:
             fieldnames = ['id', 'title', 'body']
