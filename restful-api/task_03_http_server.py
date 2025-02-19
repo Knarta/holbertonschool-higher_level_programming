3188fadb5f7ff6044dee2137fe360eae51e9a8bd
knarta <charquen10@gmail.com>
2025-02-19 10:49:22 +0100

    Refactor GET request handling in HTTP server for improved clarity and structure

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,7 +4,6 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-import requests
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -23,33 +22,25 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(data).encode("utf-8"))
 
     def do_GET(self):
-        try:
-            if self.path == '/data':
-                sample_data = {
-                    "name": "John",
-                    "age": 30,
-                    "city": "New York"
-                }
-                self.send_json(sample_data)
-            elif self.path == '/':
-                self.send_text("Hello, this is a simple API!")
-            elif self.path == '/status':
-                self.send_text("OK")
-            elif self.path == '/info':
-                json_info = {
-                    "version": "1.0",
-                    "description": "A simple API built with http.server"
-                }
-                self.send_json(json_info)
-            else:
-                self.send_text("Not found", 404)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
-                self.wfile.write("Not found".encode("utf-8"))
+        if self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_json(data)
+        elif self.path == '/status':
+            self.send_text("OK")
+        elif self.path == '/info':
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"}
+            self.send_json(info)
+        elif self.path == '/':
+            self.send_text("Hello, this is a simple API!")
+        else:
+            self.send_text("Endpoint not found", 404)
 
 
-if __name__ == '__main__':
-    server_address = ('', 8000)
-    httpd = HTTPServer(server_address, RequestHandler)
-    print("Starting server on port 8000...")
+if __name__ == "__main__":
+    host = "localhost"
+    port = 8000
+    httpd = HTTPServer((host, port), RequestHandler)
+    print("Starting server on http://localhost:8000")
     httpd.serve_forever()
