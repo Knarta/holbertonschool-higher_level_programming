16dab377fee49c13976c96b11bd0c8177e385d2f
knarta <charquen10@gmail.com>
2025-02-19 10:42:40 +0100

    Remove redundant '/info' endpoint implementation from the HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -41,17 +41,6 @@ class RequestHandler(BaseHTTPRequestHandler):
                     "description": "A simple API built with http.server"
                 }
                 self.send_json(json_info)
-            elif self.path == '/info':
-                info = {
-                    "version": "1.0",
-                    "description": "A simple API built with http.server"}
-                self.send_json(info)
-                json_info = json.dumps(info)
-                self.send_json(json_info)
-                self.send.response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json_info.encode("utf-8"))
             else:
                 self.send_text("Not found", 404)
                 self.send_header("Content-type", "text/html")
