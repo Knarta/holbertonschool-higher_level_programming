a43bac65e09c0c0be0281771af2788d29a0c528c
knarta <charquen10@gmail.com>
2025-03-07 15:37:05 +0100

    "Add script to list all State objects containing the letter 'a' from database"

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,7 +4,7 @@
 'a' from the database hbtn_0e_6_usa"""
 
 import sys
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
