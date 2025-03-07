03a56dbf04ad513886dd0c8562f2c7148b144fc1
knarta <charquen10@gmail.com>
2025-03-07 11:52:58 +0100

    "Modified the code because pycodestyle is fail"

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+
 """
- This script connects to a MySQL database, retrieves all states sorted by ID in ascending order,
+ This script connects to a MySQL database, retrieves all states sorted
+ by ID in ascending order,
  and prints the results to the console
 """
 
 import sys
 import MySQLdb
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3],
-    )
+                         db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     row_result = cursor.fetchall()
