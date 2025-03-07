8fd22c1f2b9f6d3c3896da467e748c72e6e0d508
knarta <charquen10@gmail.com>
2025-03-07 16:01:19 +0100

    "change format"

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,27 +1,20 @@
+
 #!/usr/bin/python3
 
-"""Prints all City objects from the database hbtn_0e_14_usa"""
+"""Module that defines the City class linking to the MySQL table 'cities'"""
 
-import sys
-from model_state import State
-from model_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, database))
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    cities = session.query(City, State).filter(City.state_id == State.id)\
-        .order_by(City.id).all()
-    for city, state in cities:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-
-    session.close()
+class City(Base):
+    """City class"""
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False
+                )
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
