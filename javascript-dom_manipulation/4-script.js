c9ca263a8095cd5f90b15082cdd115dc4bd697a5
knarta <charquen10@gmail.com>
2025-04-02 15:40:29 +0200

    Added event listener to #add_item

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,4 +1,4 @@
-document.querySelector("#add_item").addEventListener("click", () {
+document.querySelector("#add_item").addEventListener("click", () => {
     const list = document.querySelector("my_list");
     const newElement = document.createElement("li");
     newElement.textContent = "Item";
