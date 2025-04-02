56dde6c5079ab9863e326ef0dff8d3d12a71e10c
knarta <charquen10@gmail.com>
2025-04-02 15:39:28 +0200

    Add new list item to my_list on button click

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+document.querySelector("#add_item").addEventListener("click", () {
+    const list = document.querySelector("my_list");
+    const newElement = document.createElement("li");
+    newElement.textContent = "Item";
+    list.appendChild(newElement);
+});
